5ee4af8b6ec7fe9b14bf9ab8bc9c7400e4c168e5
Rachad <rachadaccoumeh@gmailcom>
2023-04-09 22:32:03 +0300

    test the cd

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -16,23 +16,23 @@ void main() {
     await tester.pumpWidget(const MyApp());
 
     // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    expect(find.text('1'), findsOneWidget);
+    expect(find.text('2'), findsNothing);
 
     // Tap the '+' icon and trigger a frame.
     await tester.tap(find.byIcon(Icons.add));
     await tester.pump();
 
     // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(find.text('1'), findsNothing);
+    expect(find.text('2'), findsOneWidget);
 
 
     await tester.tap(find.byIcon(Icons.add));
     await tester.pump();
 
     // Verify that our counter has incremented.
-    expect(find.text('1'), findsNothing);
-    expect(find.text('2'), findsOneWidget);
+    expect(find.text('2'), findsNothing);
+    expect(find.text('3'), findsOneWidget);
   });
 }
